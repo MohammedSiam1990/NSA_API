946b40eab51ccb5d52659c7348ec77607aa31fc1
Mohammed Hejazi <mohd_tariq_hejazi@hotmail.com>
2020-09-21 14:14:13 +0300

    GetLookUps

--- POS.Data/Entities/Lookups.cs
+++ POS.Data/Entities/Lookups.cs
@@ -5,13 +5,6 @@ namespace POS.Entities
 {
     public partial class Lookups
     {
-        public int LookupId { get; set; }
-        public string Name { get; set; }
-        public string NameAr { get; set; }
-        public string Tag { get; set; }
-        public string Value { get; set; }
-        public int? Sequence { get; set; }
-        public bool? IsDefault { get; set; }
-        public int? StatusId { get; set; }
+        public string Country_City_Currency { get; set; }
     }
 }
--- POS.Data/Repository/LookUpRepository.cs
+++ POS.Data/Repository/LookUpRepository.cs
@@ -7,6 +7,7 @@ using POS.Data.IRepository;
 using POS.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace POS.Data.Repository
@@ -25,10 +26,10 @@ namespace POS.Data.Repository
             using (var DbContext = new PosDbContext())
             {
 
-                string Sql = "EXEC Get_Json_Lookups @Lang";
-                string data= DbContext.Database.ExecuteSqlCommand(Sql, new SqlParameter("@Lang", Lang)).ToString();
+                string Sql = "EXEC Get_Json_Lookups @langID";
+                var data= DbContext.GetLookups.FromSqlRaw(Sql, new SqlParameter("@langID", Lang)).AsEnumerable().FirstOrDefault().Country_City_Currency;
 
-                return data;
+                return data.ToString();
             }
 
         }
