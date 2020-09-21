d3bcb240b3d3e4339bd04fd8ec2c97f3b1b8d953
Mohammed Hejazi <mohd_tariq_hejazi@hotmail.com>
2020-09-21 10:17:27 +0300

    LookUpsData

---
+++ POS.Data/Repository/LookUpRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using POS.Data.DataContext;
+using POS.Data.Dto.Account;
+using POS.Data.Infrastructure;
+using POS.Data.IRepository;
+using POS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POS.Data.Repository
+{
+    public class LookUpRepository : Repository<Lookups>, IlookUpRepository
+    {
+        public LookUpRepository(IDatabaseFactory databaseFactory)
+        : base(databaseFactory)
+        {
+
+        }
+
+        [Obsolete]
+        public string GetProcLookUp(string Lang)
+        {
+            using (var DbContext = new PosDbContext())
+            {
+
+                string Sql = "EXEC Get_Json_Lookups @Lang";
+                return DbContext.GetBranches.FromSqlRaw(Sql, new SqlParameter("@Lang", Lang)).ToString();
+            }
+
+        }
+    }
+}
