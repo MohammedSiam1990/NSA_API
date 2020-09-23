6b425180932a90d6daa0c09d21482d7eb8de00c6
Mohammed Hejazi <mohd_tariq_hejazi@hotmail.com>
2020-09-23 13:40:42 +0300

    BrandController

--- POS.API/Controllers/BrandController.cs
+++ POS.API/Controllers/BrandController.cs
@@ -89,6 +89,11 @@ namespace POS.API.CORE.Controllers
                 var data = BrandService.SaveProcBrands(Brand);
                 if (data != 1)
                 {
+                    if (data == -1)
+                    {
+                        return Ok(new { success = false, message = lang.An_error_occurred_while_processing_your_request});
+
+                    }
                     if (data == -2)
                     {
                         return Ok(new { success = false, message =lang.English_name_already_exists, repeated = "BrandName" });
