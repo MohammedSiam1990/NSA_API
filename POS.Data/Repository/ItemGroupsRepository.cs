8d96eeaf1075a97ee39def2357fb38bd11e0956f
Mohammed Hejazi <mohd_tariq_hejazi@hotmail.com>
2020-09-24 15:36:55 +0300

    ItemGroup Image

--- POS.Data/Repository/ItemGroupsRepository.cs
+++ POS.Data/Repository/ItemGroupsRepository.cs
@@ -29,7 +29,7 @@ namespace POS.Data.Repository
                 {
                     string Sql = "EXEC SaveItemGroups @ItemGroupID, @ItemGroupNum, @ItemGroupName," +
                         " @ItemGroupNameAr ,@ItemGroupMobileName" + ", @ItemGroupMobileNameAr, @BrandID," +
-                        " @StatusID, @TypeID,  @CreateDate, @InsertedBy" + ", @LastModifyDate,  @ModifiedBy, @GroupColor";
+                        " @StatusID, @TypeID,  @CreateDate, @InsertedBy" + ", @LastModifyDate,  @ModifiedBy, @GroupColor,@ImageName";
                     int result = DbContext.ReturnResult.FromSqlRaw(Sql,
                                        new object[] {
                                       new SqlParameter("@ItemGroupID", itemGroup.ItemGroupId),
@@ -45,7 +45,9 @@ namespace POS.Data.Repository
                                       new SqlParameter("@InsertedBy",  itemGroup.InsertedBy ?? (object)DBNull.Value)   ,
                                       new SqlParameter("@LastModifyDate",  itemGroup.LastModifyDate ?? (object)DBNull.Value)  ,
                                       new SqlParameter("@ModifiedBy", itemGroup.ModifiedBy ?? (object)DBNull.Value),
-                                      new SqlParameter("@GroupColor", itemGroup.GroupColor ?? (object)DBNull.Value)}).AsEnumerable().FirstOrDefault().ReturnValue;
+                                      new SqlParameter("@GroupColor", itemGroup.GroupColor ?? (object)DBNull.Value),
+                                      new SqlParameter("@ImageName", itemGroup.ImageName ?? (object)DBNull.Value)
+                                       }).AsEnumerable().FirstOrDefault().ReturnValue;
                     return result;
                 }
                 catch (Exception ex)
