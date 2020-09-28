dcf1b941b816a6849529ecd8dfe4bca7f8780b3f
Arqami-DevOps <Arqami-hr@hotmail.com>
2020-09-28 10:57:56 +0200

    update entites

---
+++ POS.Data/Entities/ItemUom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POS.Data.Entities
+{
+    public partial class ItemUom
+    {
+        public long ItemUomid { get; set; }
+        public long ItemId { get; set; }
+        public int Uomid { get; set; }
+        public string Name { get; set; }
+        public string NameAr { get; set; }
+        public decimal? Cost { get; set; }
+        public decimal? Price { get; set; }
+        public bool? Sell { get; set; }
+        public bool? Transfer { get; set; }
+        public bool? Adjust { get; set; }
+        public bool? Component { get; set; }
+        public bool? Purchase { get; set; }
+        public decimal? Equivalent { get; set; }
+    }
+}
---
+++ POS.Data/Entities/Sku.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POS.Data.Entities
+{
+    public partial class Sku
+    {
+        public long? Skuid { get; set; }
+        public long? ItemUomid { get; set; }
+        public string Code { get; set; }
+    }
+}
