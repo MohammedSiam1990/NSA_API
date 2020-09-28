83b952c03d21c378fad154411471b9bd32026030
ehab shqair <e_shqair6@hotmail.com>
2020-09-28 08:43:41 +0300

    add try catch

--- POS.Service/Services/MailService.cs
+++ POS.Service/Services/MailService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Exceptions;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Net;
 using System.Net.Mail;
@@ -18,21 +19,22 @@ namespace POS.Service.Services
 
         public bool SendEmailAsync(string Smtp, int Port, bool EnableSsl, string From, string To, string Subject, string body, string CredentialEmail, string CredentialPassword)
         {
-            MailAddress to = new MailAddress(To);
-            MailAddress from = new MailAddress(From);
-
-            MailMessage message = new MailMessage(from, to);
-            message.Subject = Subject;
-            message.Body = body;
-            message.IsBodyHtml = true;
-            SmtpClient client = new SmtpClient
-            {
-                Host = Smtp,//"smtp.gmail.com",
-                Port = Port,
-                EnableSsl = EnableSsl
-            };
             try
             {
+                MailAddress to = new MailAddress(To);
+                MailAddress from = new MailAddress(From);
+
+                MailMessage message = new MailMessage(from, to);
+                message.Subject = Subject;
+                message.Body = body;
+                message.IsBodyHtml = true;
+                SmtpClient client = new SmtpClient
+                {
+                    Host = Smtp,//"smtp.gmail.com",
+                    Port = Port,
+                    EnableSsl = EnableSsl
+                };
+
                 client.UseDefaultCredentials = false;
                 client.Credentials = new NetworkCredential(CredentialEmail, CredentialPassword);
                 client.Send(message);
@@ -40,9 +42,10 @@ namespace POS.Service.Services
             }
             catch (SmtpException ex)
             {
-                Console.WriteLine(ex.ToString());
-                return false;
+                ExceptionError.SaveException(ex);
+                
             }
+            return false;
         }
 
         //public Task SendEmailAsync(string From, string To, string Subject, string body, string CredentialEmail, string CredentialPasswordt)
