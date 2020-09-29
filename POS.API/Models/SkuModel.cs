b0ec8afc88fc583a6ca61feeb69b717d3a5d6683
ahmed Eltyep <eltyepahmed8@gmail.com>
2020-09-29 13:26:02 +0200

    add skumodel

---
+++ POS.API/Models/SkuModel.cs
@@ -0,0 +1,16 @@
+﻿using POS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POS.API.Models
+{
+    public class SkuModel
+    {
+        public long? Skuid { get; set; }
+        public string Code { get; set; }
+        public long? ItemUomid { get; set; }
+        public ItemUom ItemUom { get; set; }
+    }
+}
