7fd3f9109d8befc3ace92993c23663497e6e01ce
Arqami-DevOps <Arqami-hr@hotmail.com>
2020-09-30 13:50:26 +0200

    insert  entity

---
+++ POS.Data/Entities/RemarksTemplateDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POS.Entities
+{
+    public partial class RemarksTemplateDetails
+    {
+        public int RemarksTemplateDetailsD { get; set; }
+        public int RemarksTemplateId { get; set; }
+        public string RemarkName1 { get; set; }
+        public string RemarkName2 { get; set; }
+        public int TypeId { get; set; }
+        public int? ItemId { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? Calories { get; set; }
+    }
+}
