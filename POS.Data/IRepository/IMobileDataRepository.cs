239d75e5ab04d42577c55e8133cd804ca40ae58d
Mohammed Hejazi <mohd_tariq_hejazi@hotmail.com>
2020-10-05 12:15:50 +0300

    Add Items Data To mobile DataList

--- POS.Data/IRepository/IMobileDataRepository.cs
+++ POS.Data/IRepository/IMobileDataRepository.cs
@@ -7,6 +7,6 @@ namespace POS.Data.IRepository
 {
     public interface IMobileDataRepository
     {
-        string GetMobileData(int CompanyID, string BrandImageURL, string BranchImageURL, string ItemGroupImageURL);
+        string GetMobileData(int CompanyID, string BrandImageURL, string BranchImageURL, string ItemGroupImageURL, string ItemImageURL);
     }
 }
