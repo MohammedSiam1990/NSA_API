df27271afb3d8eebfa1ddfd30af1e26b51aad104
Mohammed Hejazi <mohd_tariq_hejazi@hotmail.com>
2020-10-07 10:30:52 +0300

    Add mobile Controller

--- POS.Data/Repository/MobileDataRepository.cs
+++ POS.Data/Repository/MobileDataRepository.cs
@@ -21,21 +21,44 @@ namespace POS.Data.Repository
         }
 
         [Obsolete]
-        public string GetMobileData(int CompanyID, string BrandImageURL, string BranchImageURL, string ItemGroupImageURL,string ItemImageURL)
+        public int CheckSerialNumber(int CompanyID, string Serial)
         {
             using (var DbContext = new PosDbContext())
             {
                 try
                 {
-                    
+                    string Sql = "EXEC CheckSerialNumber @CompanyID , @Serial";
+                    int result = DbContext.ReturnResult.FromSqlRaw(Sql,
+                                       new object[] {
+                                      new SqlParameter("@CompanyID", CompanyID ),
+                                      new SqlParameter("@Serial", Serial )}).AsEnumerable().FirstOrDefault().ReturnValue;
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    Exceptions.ExceptionError.SaveException(ex);
+                }
+                return -1;
+            }
+
+        }
+
+        [Obsolete]
+        public string GetMobileData(int CompanyID, string BrandImageURL, string BranchImageURL, string ItemGroupImageURL, string ItemImageURL)
+        {
+            using (var DbContext = new PosDbContext())
+            {
+                try
+                {
+
                     string Sql = "EXEC Get_all_data_json @CompanyID,@BrandImageURL,@BranchImageURL,@ItemGroupImageURL,@ItemImageURL";
                     DbContext.Database.SetCommandTimeout(0);
-                     var data = DbContext.JsonData.FromSqlRaw(Sql,
-                        new SqlParameter("@CompanyID", CompanyID),
-                        new SqlParameter("@BrandImageURL", BrandImageURL),
-                        new SqlParameter("@BranchImageURL", BranchImageURL),
-                        new SqlParameter("@ItemGroupImageURL", ItemGroupImageURL),
-                        new SqlParameter("@ItemImageURL", ItemImageURL)).AsEnumerable().FirstOrDefault().Data;
+                    var data = DbContext.JsonData.FromSqlRaw(Sql,
+                       new SqlParameter("@CompanyID", CompanyID),
+                       new SqlParameter("@BrandImageURL", BrandImageURL),
+                       new SqlParameter("@BranchImageURL", BranchImageURL),
+                       new SqlParameter("@ItemGroupImageURL", ItemGroupImageURL),
+                       new SqlParameter("@ItemImageURL", ItemImageURL)).AsEnumerable().FirstOrDefault().Data;
 
                     return data.ToString();
                 }
@@ -45,6 +68,29 @@ namespace POS.Data.Repository
                 }
                 return null;
 
+            }
+        }
+
+        [Obsolete]
+        public int UpdateSerialNumber(int CompanyID, string Serial, string Mac)
+        {
+            using (var DbContext = new PosDbContext())
+            {
+                try
+                {
+                    string Sql = "EXEC UpdateSerialNumber @CompanyID , @Serial,@Mac";
+                    int result = DbContext.ReturnResult.FromSqlRaw(Sql,
+                                       new object[] {
+                                      new SqlParameter("@CompanyID", CompanyID ),
+                                      new SqlParameter("@Mac", Mac ),
+                                      new SqlParameter("@Serial", Serial )}).AsEnumerable().FirstOrDefault().ReturnValue;
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    Exceptions.ExceptionError.SaveException(ex);
+                }
+                return -1;
             }
         }
     }
--- POS.Service/IService/IMobileDataService.cs
+++ POS.Service/IService/IMobileDataService.cs
@@ -5,6 +5,9 @@ namespace POS.Service.IService
 {
     public interface IMobileDataService
     {
-        string GetMobileData(int CompanyID, string BrandImageURL, string BranchImageURL, string ItemGroupImageURL,string ItemImageURL);
+        string GetMobileData(int CompanyID, string BrandImageURL, string BranchImageURL, string ItemGroupImageURL, string ItemImageURL);
+        int UpdateSerialNumber(int CompanyID, string Serial, string Mac);
+        int CheckSerialNumber(int CompanyID, string Serial);
+
     }
 }
