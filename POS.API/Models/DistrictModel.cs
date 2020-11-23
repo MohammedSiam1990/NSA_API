37cdeae2c4c4dd3703e7638d2e8214fcd70a6fdc
ahmed Eltyep <eltyepahmed8@gmail.com>
2020-11-23 10:40:54 +0200

    update1

--- POS.API/Models/DistrictModel.cs
+++ POS.API/Models/DistrictModel.cs
@@ -15,7 +15,6 @@ namespace POS.API.Models
     public string DistrictNameAr { get; set; }
     public int CityId { get; set; }
     public bool? InActive { get; set; }
-    public string CityCode { get; set; }
     public string CityName { get; set; }
     public string CityNameAr { get; set; }
   }
