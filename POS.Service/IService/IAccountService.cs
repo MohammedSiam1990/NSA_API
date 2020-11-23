aa64b6ad5e6699af352e3af546c726e007b5a191
Mohammed Hejazi <mohd_tariq_hejazi@hotmail.com>
2020-11-23 14:20:07 +0200

    CreateUser

--- POS.Service/IService/IAccountService.cs
+++ POS.Service/IService/IAccountService.cs
@@ -1,4 +1,5 @@
 ﻿using POS.Data.Dto;
+using POS.Data.Dto.Account;
 using Steander.Core.DTOs;
 using Steander.Core.Entities;
 using System;
@@ -11,6 +12,7 @@ namespace Pos.IService
     {
 
         Task<UserManagerResponse> RegisterUserAsync(RegisterViewModel model);
+        Task<UserManagerResponse> CreateUserAsync(CreateUserModel model);
 
         Task<LoginResponseDto> LoginUserAsync(LoginViewModel model);
 
--- POS.Service/Services/AccountService.cs
+++ POS.Service/Services/AccountService.cs
@@ -6,6 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 using Pos.IService;
 using POS.Core.Resources;
 using POS.Data.Dto;
+using POS.Data.Dto.Account;
 using POS.Entities;
 using POS.IService.Base;
 using Steander.Core.DTOs;
@@ -21,6 +22,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Security;
 
 namespace Pos.Service
 {
@@ -591,6 +593,141 @@ namespace Pos.Service
         {
             var user = await _userManger.FindByIdAsync(userName);
             return user;
+        }
+
+        public async Task<UserManagerResponse> CreateUserAsync(CreateUserModel model)
+        {
+            try
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(model.Lang);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(model.Lang);
+
+                if (model == null)
+                {
+                    return new UserManagerResponse
+                    {
+                        message = lang.Reigster_Model_is_null,
+                        success = false,
+                    };
+                }
+
+                if (string.IsNullOrWhiteSpace(model.Email))
+                {
+                    return new UserManagerResponse
+                    {
+                        message = lang.Missing_username,
+                        success = false,
+                    };
+                }
+
+
+                var appUser = await _userManger.FindByEmailAsync(model.Email);
+                if (appUser != null && appUser.Email == model.Email)
+                {
+                    return new UserManagerResponse
+                    {
+                        message = lang.Either_username_and_or_email_already_exists,
+                        success = false,
+                    };
+                }
+
+                appUser = await _userManger.FindByNameAsync(model.Username);
+                if (appUser != null && appUser.UserName == model.Username)
+                {
+                    return new UserManagerResponse
+                    {
+                        message = lang.Either_username_and_or_email_already_exists,
+                        success = false,
+                    };
+
+                }
+
+
+                var identityUser = new ApplicationUser()
+                {
+                    UserName = model.Username,
+                    Email = model.Email,
+                    CreateDate = DateTime.Now,
+                    Name = model.Name,
+                    PhoneNumber = model.PhoneNumber,
+                    EmailConfirmed = true,
+                    IsSuperAdmin = false,
+                    LockoutEnabled = false,
+                    CompanyId = null,
+                    InsertedBy=model.InsertedBy,
+                    UserType = model.UserType,
+                };
+
+                string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+                Random random = new Random();
+                string Password="";
+                for (int i = 0; i < 6; i++)
+                {
+                    Password += validChars[random.Next(0, validChars.Length)];
+                }
+                identityUser.Password = Password.ToString();
+                var result = await _userManger.CreateAsync(identityUser, Password.ToString());
+
+
+                if (result.Succeeded)
+                {
+                    var confirmEmailToken = await _userManger.GenerateEmailConfirmationTokenAsync(identityUser);
+
+                    var encodedEmailToken = Encoding.UTF8.GetBytes(confirmEmailToken);
+                    var validEmailToken = WebEncoders.Base64UrlEncode(encodedEmailToken);
+
+                    var User = await _userManger.FindByEmailAsync(model.Email);
+
+                    string code = await this._userManger.GenerateEmailConfirmationTokenAsync(User);
+                    var callbackUrl = model.AppUrl + "?userId=" + User.Id + "&code=" + code + "&lang=" + model.Lang;
+
+
+                    //var Body = lang.Please_activate_your_account_by_clicking + "<a href=\"" + callbackUrl + "\"> " + lang.Here + "</a>";
+                                string Body = "<html xmlns='http://www.w3.org/1999/xhtml'><head>    <meta http-equiv='content-type' content='text/html; charset=utf-8'>    <meta name='viewport' content='width=device-width, initial-scale=1.0;'>    <meta name='format-detection' content='telephone=no' />    <style>        body {            margin: 0;            padding: 0;            min-width: 100%;            width: 100% !important;            height: 100% !important;        }                body,        table,        td,        div,        p,        a {            -webkit-font-smoothing: antialiased;            text-size-adjust: 100%;            -ms-text-size-adjust: 100%;            -webkit-text-size-adjust: 100%;            line-height: 100%;        }                table,        td {            mso-table-lspace: 0pt;            mso-table-rspace: 0pt;            border-collapse: collapse !important;            border-spacing: 0;        }                img {            border: 0;            line-height: 100%;            outline: none;            text-decoration: none;            -ms-interpolation-mode: bicubic;        }                #outlook a {            padding: 0;        }                .ReadMsgBody {            width: 100%;        }                .ExternalClass {            width: 100%;        }                .ExternalClass,        .ExternalClass p,        .ExternalClass span,        .ExternalClass font,        .ExternalClass td,        .ExternalClass div {            line-height: 100%;        }                @media all and (min-width: 560px) {            .container {                border-radius: 8px;                -webkit-border-radius: 8px;                -moz-border-radius: 8px;                -khtml-border-radius: 8px;            }        }                a,        a:hover {            color: #0a5c80;        }                .footer a,        .footer a:hover {            color: #828999;        }    </style>    <title></title></head><body topmargin='0' rightmargin='0' bottommargin='0' leftmargin='0' marginwidth='0' marginheight='0' width='100%' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%; height: 100%; -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%;	background-color: #ffff;	color: #0a5c80;' bgcolor='#fff' text='##0a5c80'>    <table width='100%' align='center' border='0' cellpadding='0' cellspacing='0' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%;' class='background'>        <tr>            <td align='center' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;' bgcolor='#fff'>                <table border='0' cellpadding='0' cellspacing='0' align='center' width='500' style='border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;	max-width: 500px;' class='wrapper'>                    <tr>                        <td align='center' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;			padding-top: 20px;			padding-bottom: 20px;'>                            <div style='display: none; visibility: hidden; overflow: hidden; opacity: 0; font-size: 1px; line-height: 1px; height: 0; max-height: 0; max-width: 0;				color: #2D3445;' class='preheader'>                            </div>                        </td>                    </tr>                    <tr>                        <td align='center' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;			padding-top: 0px;' class='hero'>                            <a target='_blank' style='text-decoration: none;' href='#'><img border='0' vspace='0' hspace='0' src='http://pos.opos.me/assets/images/arqami_logo.svg' alt='Please enable images to view this content' title='Hero Image' width='340' style='			width: 87.5%;			max-width: 340px;			color: #0a5c80; font-size: 13px; margin: 0; padding: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; border: none; display: block;' /></a>                        </td>                    </tr>                    <tr>                        <td align='center' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 14px; font-weight: 400; line-height: 150%; letter-spacing: 2px;			padding-top: 27px;			padding-bottom: 0;			color: #0a5c80;			font-family: sans-serif;' class='supheader'>                            Welcome To                        </td>                    </tr>                    <tr>                        <td align='center' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;  padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%;			padding-top: 5px;			color: #0a5c80;			font-family: sans-serif;' class='header'>                            POS Arqami System                        </td>                    </tr>                    <tr>                        <td align='right' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;			padding-top: 15px; 			color: #0a5c80;            font-family: sans-serif; text-align: center;' class='paragraph'>Dear Client, your Password is:                            <b class='text-center'></b>                        </td>                    </tr>                    <tr>                        <td align='center' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;			padding-top: 25px;			padding-bottom: 5px;' class='button'>                            <a target='_blank' style='text-decoration: underline;'>                                <table border='0' cellpadding='0' cellspacing='0' align='center' style='max-width: 400px; min-width: 120px; border-collapse: collapse; border-spacing: 0; padding: 0;'>                                    <tr>                                        <td  align='center' valign='middle' style='padding: 12px 24px;color: white;font-weight: bold;                                        margin: 0;  border-collapse: collapse; border-spacing: 0; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; -khtml-border-radius: 4px;' bgcolor='#0a5c80'>                                            " + Password + "                                        </td>                                    </tr>                                </table>                            </a>                        </td>                    </tr>                    <tr>                        <td align='center' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;			padding-top: 30px;' class='line'>                            <hr color='#565F73' align='center' width='100%' size='1' noshade style='margin: 0; padding: 0;' />                        </td>                    </tr>                    <tr>                        <td align='center' valign='top' style='border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 13px; font-weight: 400; line-height: 150%;			padding-top: 10px;			padding-bottom: 20px;			color: #828999;			font-family: sans-serif;' class='footer'>                            You are receiving this email because of your relationship with POS Arqami . Please reconfirm your interest in receiving emails from us.If you do not wish to receive any more emails, you can unsubscribe here.                            <img width='1' height='1' border='0' vspace='0' hspace='0' style='margin: 0; padding: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; border: none; display: block;' src='https://raw.githubusercontent.com/konsav/email-templates/master/images/tracker.png'                            />                        </td>                    </tr>                </table>            </td>        </tr>    </table></body></html>";
+
+                    var Subject = lang.Your_registration_completed_successfully;
+
+                    //string url = $"{emailConfig.AppUrl}/api/auth/confirmemail?userid={identityUser.Id}&token={validEmailToken}";
+                    bool isMessageSent = mailService.SendEmailAsync(emailConfig.SmtpServer, emailConfig.Port, emailConfig.EnableSsl, emailConfig.From, identityUser.Email, Subject, Body, emailConfig.From, emailConfig.Password, emailConfig.UseDefaultCredentials);
+
+                    if (isMessageSent == false)
+                    {
+                        try
+                        {
+                            var response = await DeletetUserAsync(User.Id);
+                        }
+                        catch (Exception ex)
+                        {
+
+                            ExceptionError(ex);
+                        }
+                        return new UserManagerResponse
+                        {
+                            message = lang.Cant_send_activation_email_please_try_registration_later,
+                            success = false,
+                        };
+
+                    }
+
+                    return new UserManagerResponse
+                    {
+                        message = lang.Your_registration_completed_successfully + ", " + lang.Please_check_your_email_to_activtate_your_email,
+                        //EmailConfirmed = User.EmailConfirmed,
+                        success = true,
+                    };
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ExceptionError(ex);
+            }
+            return new UserManagerResponse
+            {
+                message = lang.An_error_occurred_while_processing_your_request,
+                success = false,
+            };
+
         }
     }
 }
