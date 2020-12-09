94fd42499a786537533970046d6ae700d77236a8
ehab shqair <e_shqair6@hotmail.com>
2020-12-09 09:12:44 +0200

    qqqq

---
+++ POS.Data/Repository/PriceTemplateDetailsRepository.cs
@@ -0,0 +1,35 @@
+﻿using POS.Data.DataContext;
+using POS.Data.Entities;
+using POS.Data.Infrastructure;
+using POS.Data.IRepository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace POS.Data.Repository
+{
+    public class PriceTemplateDetailsRepository : Repository<PriceTemplateDetails>, IPriceTemplateDetailsRepository
+    {
+        public PriceTemplateDetailsRepository(IDatabaseFactory databaseFactory) : base(databaseFactory)
+        {
+        }
+
+        public void DeletePriceTemplateDetails(int PriceTemplateID, List<PriceTemplateDetails> model)
+        {
+            using (var context = new PosDbContext())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    if (PriceTemplateID != 0)
+                    {
+                        var OldPrice = GetMany(e => e.PriceTemplateID== PriceTemplateID).ToList();
+                        base.DeleteRange(OldPrice);
+                        transaction.Commit();
+
+                    }
+                    //base.AddRange(model);
+                }
+            }
+        }
+
+    }
+}
