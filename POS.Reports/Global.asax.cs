415fe8d57613dbb853fa97c72d607dc62d8a3edb
Mohammed Hejazi <mohd_tariq_hejazi@hotmail.com>
2020-12-09 13:34:47 +0200

    ValidateNameAlreadyExist

--- POS.Reports/Global.asax.cs
+++ POS.Reports/Global.asax.cs
@@ -2,16 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
 
 namespace POS.Reports
 {
-    public class WebApiApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
-    }
+	public class Global : System.Web.HttpApplication
+	{
+
+		protected void Application_Start(object sender, EventArgs e) 		
+		{
+            Telerik.Reporting.Services.WebApi.ReportsControllerConfiguration.RegisterRoutes(System.Web.Http.GlobalConfiguration.Configuration);
+            Telerik.WebReportDesigner.Services.WebApi.ReportDesignerControllerConfiguration.RegisterRoutes(System.Web.Http.GlobalConfiguration.Configuration);
+
+		}
+		
+		protected void Session_Start(object sender, EventArgs e) 
+		{
+
+		}
+
+		protected void Application_BeginRequest(object sender, EventArgs e) 
+		{
+
+		}
+
+		protected void Application_AuthenticateRequest(object sender, EventArgs e) 
+		{
+
+		}
+
+		protected void Application_Error(object sender, EventArgs e) 
+		{
+
+		}
+
+		protected void Session_End(object sender, EventArgs e) 
+		{
+
+		}
+
+		protected void Application_End(object sender, EventArgs e) 
+		{
+
+		}
+	}
 }
